a24f72ddf8f99f8fde0b9eb98ddd3f237524d506
renanl15 <renanalmeida201612@gmail.com>
2023-09-23 00:42:36 -0300

    update: planos

--- src/components/Planos/Planos.jsx
+++ src/components/Planos/Planos.jsx
@@ -18,35 +18,31 @@ export default function Planos() {
             </h2>
             <p>Por mês</p>
           </div>
-          <ul className="flex flex-col gap-8 mt-5">
-            <li className="font-['Open_Sans']">
+          <ul className="flex flex-col gap-8 pr-28 mt-5 py-4 font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Pode fazer isso.
+              Armazenamento ilimitado
             </li>
-            <li className="font-['Open_Sans']">
-              <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Você também pode fazer isso.
-            </li>
-            <li className="font-['Open_Sans'] text-[rgba(0,0,0,0.5)]">
+            <li className=" text-[rgba(0,0,0,0.5)]">
               <i className="fa-regular fa-circle-xmark text-red-800 mr-3"></i>
-              Isso também você pode fazer.
+              Gerador de relatórios
             </li>
-            <li className="font-['Open_Sans'] text-[rgba(0,0,0,0.5)]">
+            <li className=" text-[rgba(0,0,0,0.5)]">
               <i className="fa-regular fa-circle-xmark text-red-800 mr-3"></i>
-              Saiba que você pode fazer isso se quiser.
+              Relatórios avançados
             </li>
-            <li className="font-['Open_Sans'] text-[rgba(0,0,0,0.5)]">
+            <li className=" text-[rgba(0,0,0,0.5)]">
               <i className="fa-regular fa-circle-xmark text-red-800 mr-3"></i>
-              Não esquece que você também pode fazer isso.
+              Gerenciamento pelo aplicativo
             </li>
           </ul>
           <button
             onClick={() => navigate("/signup")}
-            className="m-auto mt-10 py-2 px-7 rounded-xl bg-slate-900 text-white text-3xl font-['Open_Sans'] font-bold duration-200 hover:scale-105">
+            className="m-auto mt-10 py-2 px-7 rounded-xl bg-slate-900 text-white text-3xl  font-bold duration-200 hover:scale-105">
             Obter
           </button>
         </div>
-        <div className="flex flex-col px-14 py-16 -translate-y-6 bg-white shadow-2xl shadow-[rgba(0,0,0,0.16)] rounded-2xl mt-10 bg-gradient-to-tr from-[#d6fcff] to-[#efffd3]">
+        <div className="flex flex-col px-14 py-16 -translate-y-8 bg-white shadow-2xl shadow-[rgba(0,0,0,0.16)] rounded-2xl mt-10 bg-gradient-to-tr from-[#d6fcff] to-[#efffd3]">
           <div className="flex flex-col items-center gap-3">
             <div className="flex gap-4 items-center">
               <h1 className="font-['PT_Sans'] text-5xl max-2xl:text-4xl">
@@ -59,26 +55,22 @@ export default function Planos() {
             </h2>
             <p>Por mês</p>
           </div>
-          <ul className="flex flex-col gap-8 mt-5">
-            <li className="font-['Open_Sans']">
+          <ul className="flex flex-col gap-8 mt-5 py-4 font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Pode fazer isso.
+              Armazenamento ilimitado
             </li>
-            <li className="font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Você também pode fazer isso.
+              Gerador de relatórios
             </li>
-            <li className="font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Isso também você pode fazer.
+              Relatórios avançados
             </li>
-            <li className="font-['Open_Sans'] text-[rgba(0,0,0,0.5)]">
+            <li className=" text-[rgba(0,0,0,0.5)]">
               <i className="fa-regular fa-circle-xmark text-red-800 mr-3"></i>
-              Saiba que você pode fazer isso se quiser.
-            </li>
-            <li className="font-['Open_Sans'] text-[rgba(0,0,0,0.5)]">
-              <i className="fa-regular fa-circle-xmark text-red-800 mr-3"></i>
-              Não esquece que você também pode fazer isso.
+              Gerenciamento pelo aplicativo
             </li>
           </ul>
           <button
@@ -89,32 +81,28 @@ export default function Planos() {
         </div>
         <div className="flex flex-col p-10 bg-white shadow-2xl shadow-[rgba(0,0,0,0.16)] rounded-2xl mt-10">
           <div className="flex flex-col items-center gap-3">
-            <h1 className="font-['PT_Sans'] text-5xl">Plano Básico</h1>
+            <h1 className="font-['PT_Sans'] text-5xl">Plano Pro</h1>
             <h2 className="font-['PT_Sans'] text-5xl">
               <span className="font-sans text-3xl">R$</span>149,99
             </h2>
             <p>Por mês</p>
           </div>
-          <ul className="flex flex-col gap-8 mt-5">
-            <li className="font-['Open_Sans']">
+          <ul className="flex flex-col gap-8 pr-28 mt-5 py-4 font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Pode fazer isso.
+              Armazenamento ilimitado
             </li>
-            <li className="font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Você também pode fazer isso.
+              Gerador de relatórios
             </li>
-            <li className="font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Isso também você pode fazer.
+              Relatórios avançados
             </li>
-            <li className="font-['Open_Sans']">
+            <li>
               <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Saiba que você pode fazer isso se quiser.
-            </li>
-            <li className="font-['Open_Sans']">
-              <i className="fa-regular fa-circle-check text-green-700 mr-3"></i>
-              Não esquece que você também pode fazer isso.
+              Gerenciamento pelo aplicativo
             </li>
           </ul>
           <button
